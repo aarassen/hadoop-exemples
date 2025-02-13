9243083e92e1482dcb5e5f5ead23b25f86d08a34
aarassen <aarassen@gmail.com>
2025-02-13 21:10:32 +0100

    Update compile java code.sh
    make compiled_out directory

--- 1.Wordcount/compile java code.sh
+++ 1.Wordcount/compile java code.sh
@@ -1,2 +1,3 @@
+mkdir compiled_out
 javac -classpath "${HADOOP_HOME}/share/hadoop/common/hadoop-common-3.4.0.jar:${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-core-3.4.0.jar:${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-common-3.4.0.jar:${HADOOP_HOME}/share/hadoop/common/lib/*" -d compiled_out/ Wordcount.java
 jar -cvf WordCount.jar -C compiled_out/ .
